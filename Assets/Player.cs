9b6a0a0c51918a3a5c2e94cd05dbf62d3a9c31fc
ViperMoto99 <sinner.gameover@gmail.com>
2022-05-24 13:52:05 +0300

    add colliders and useless shit

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerRigidBody;
     [SerializeField] float playerSpeed;
+    [SerializeField] Animator playerAnimator; 
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +23,17 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxisRaw("Vertical");
         //Get the value of the Vertical input axis.
 
-        playerRigidBody.transform.Translate(new Vector2(horizontalInput, verticalInput) * playerSpeed * Time.deltaTime);
-   
+        playerRigidBody.velocity = new Vector2(horizontalInput, verticalInput) * playerSpeed;
         //Move the object to XYZ coordinates defined as horizontalInput, 0, and verticalInput respectively.
+
+        playerAnimator.SetFloat("movementX", playerRigidBody.velocity.x);
+        playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
+        //Animator Movement Control
+        if (horizontalInput == 1 || horizontalInput == -1 || verticalInput == 1 || verticalInput == -1)
+        {
+            playerAnimator.SetFloat("lastX", horizontalInput);
+            playerAnimator.SetFloat("lastY", verticalInput);
+        }
+       //Animator Idling Control
     }
 }
